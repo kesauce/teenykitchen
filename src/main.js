496d53c001212cae81e049389e0e2f5051d3ed42
Alexis Manosca <alexismanosca@gmail.com>
2025-09-22 22:13:51 +1200

    made the fridge clickable that opens and closes

--- src/main.js
+++ src/main.js
@@ -10,17 +10,39 @@ const sizes = {
 const speedDown = 300
 
 // Setting up game scene
-class GameScene extends Phaser.Scene{
+class Kitchen extends Phaser.Scene{
   constructor(){
     super("scene-game")
   }
 
   preload(){
     this.load.image("bg", "/assets/kitchen/bg.png")
+    this.load.image("openfridge", "/assets/kitchen/openfridge.png")
   }
 
   create(){
-    this.add.image(0, 0, "bg").setOrigin(0, 0)
+    // Add the kitchen background
+    this.add.image(0, 0, "bg").setOrigin(0, 0);
+
+    // Sets the clickable fridge
+    let fridgeZone = this.add.zone(155, 13, 32, 58);
+    fridgeZone.setOrigin(0);
+    fridgeZone.setInteractive({ useHandCursor: true }); // Uses a hand cursor
+    let openFridgeImage;
+
+    fridgeZone.on('pointerdown', () => {
+      console.log("Fridge clicked!");
+      openFridgeImage = this.add.image(146, 8, "openfridge").setOrigin(0, 0);
+    });
+
+    // Check if user clicks outside interactables
+    this.input.on('pointerdown', (pointer, currentlyOver) => {
+      if (!currentlyOver.includes(fridgeZone)) {
+          console.log("Clicked outside the fridge!");
+          openFridgeImage.destroy();
+      }
+    });
+
   }
 
   update(){
@@ -50,7 +72,7 @@ const config = {
     zoom: 3              // <-- upscale by 3
   },
 
-  scene: [GameScene]
+  scene: [Kitchen]
 }
 
 // Running the game
