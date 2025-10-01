21ca021390f9ef41ffd0991a70d93ab9fb0f2d31
Alexis Manosca <alexismanosca@gmail.com>
2025-10-01 22:58:15 +1300

    changed to open fridge when hovered

--- src/fridge.js
+++ src/fridge.js
@@ -4,42 +4,31 @@ export default class Fridge {
     this.scene = scene;
     this.zone;
     this.openImage;
-    this.isOpen;
     this.menu;
 
     // Sets the clickable fridge
     this.zone = scene.add.zone(155, 13, 32, 58).setOrigin(0).setInteractive({ useHandCursor: true });
     this.openImage = scene.add.image(146, 8, "openfridge").setOrigin(0, 0).setVisible(false);
-    this.isOpen = false;
 
+    // Opens menu when clicked
     this.zone.on('pointerdown', () => {
       console.log("Fridge clicked!");
-      if (!this.isOpen){
-        this.open();
-      }
-      else{
-        this.close();
-      }
+      this.open();
+    });
+
+    // Show open fridge when hovered
+    this.zone.on('pointerover', () => {
+      this.openImage.setVisible(true);
+    });
+
+    // Hide open fridge when not hovered
+    this.zone.on('pointerout', () => {
+      this.openImage.setVisible(false);
     });
   }
 
   open() {
-    this.openImage.setVisible(true);
-    this.isOpen = true;
-
-    // Add menu container
-    this.menu = this.scene.add.container(100, 100);
-    let bg = this.scene.add.rectangle(0, 0, 200, 200, 0x000000, 0.7);
-    this.menu.add(bg);
+    this.scene.scene.launch("FridgeMenu");
   }
-  
-  close() {
-    this.openImage.setVisible(false);
-    this.isOpen = false;
 
-    if (this.menu) {
-      this.menu.destroy();
-      this.menu = null;
-    }
-  }
 }
