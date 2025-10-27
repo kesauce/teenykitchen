5a0051752d6cec46835036d3077a3e74e12ca121
Alexis Manosca <alexismanosca@gmail.com>
2025-10-27 18:04:27 +1300

    modified tab sizing to default tab size of 4

--- src/stove.js
+++ src/stove.js
@@ -1,34 +1,34 @@
 export default class Stove {
-  constructor(scene) {
+	constructor(scene) {
 
-    this.scene = scene;
-    this.zone;
-    this.openImage;
-    this.menu;
+		this.scene = scene;
+		this.zone;
+		this.openImage;
+		this.menu;
 
-    // Sets the clickable stove
-    this.zone = scene.add.zone(273, 93, 90, 48).setOrigin(0).setInteractive({ useHandCursor: true });
-    this.openImage = scene.add.image(273, 93, "stoveon").setOrigin(0, 0).setVisible(false);
+		// Sets the clickable stove
+		this.zone = scene.add.zone(273, 93, 90, 48).setOrigin(0).setInteractive({ useHandCursor: true });
+		this.openImage = scene.add.image(273, 93, "stoveon").setOrigin(0, 0).setVisible(false);
 
-    // Opens menu when clicked
-    this.zone.on('pointerdown', () => {
-      console.log("Stove clicked!");
-      this.open();
-    });
+		// Opens menu when clicked
+		this.zone.on('pointerdown', () => {
+			console.log("Stove clicked!");
+			this.open();
+		});
 
-    // Show open stove when hovered
-    this.zone.on('pointerover', () => {
-      this.openImage.setVisible(true);
-    });
+		// Show open stove when hovered
+		this.zone.on('pointerover', () => {
+			this.openImage.setVisible(true);
+		});
 
-    // Hide open stove when not hovered
-    this.zone.on('pointerout', () => {
-      this.openImage.setVisible(false);
-    });
-  }
+		// Hide open stove when not hovered
+		this.zone.on('pointerout', () => {
+			this.openImage.setVisible(false);
+		});
+	}
 
-  open() {
-    
-  }
-  
+	open() {
+
+	}
+
 }
