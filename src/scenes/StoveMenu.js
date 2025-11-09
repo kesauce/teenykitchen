ce9f461b66aa868e308411a9715040cdc3229364
Alexis Manosca <alexismanosca@gmail.com>
2025-11-09 21:52:12 +1300

    added JSDocs and cleaned up code

--- src/scenes/StoveMenu.js
+++ src/scenes/StoveMenu.js
@@ -1,4 +1,18 @@
 import Recipes from '../classes/Recipes.js'
+import Stove from '../classes/Stove.js';
+
+/**
+ * @typedef {Object} StoveMeasurements
+ * @property {number} menuX - X position of the menu.
+ * @property {number} menuY - Y position of the menu.
+ * @property {number} menuWidth - Width of the menu.
+ * @property {number} menuHeight - Height of the menu.
+ * @property {number} zoneX - X position of the first, leftmost ingredient zone.
+ * @property {number} zoneT - Y position of the first, leftmost ingredient zone.
+ * @property {number} zoneWidth - Width of each ingredient zone.
+ * @property {number} zoneHeight - Height of each ingredient zone.
+ * @property {number} zoneGap - Gap between each ingredient zone
+ */
 
 export default class StoveMenu extends Phaser.Scene {
     constructor() {
@@ -23,34 +37,126 @@ export default class StoveMenu extends Phaser.Scene {
             // [name, icon, image, popup?]
         ];
 
-        // Access global fridge object
         this.stove = this.registry.get('stove');
         this.inventory = this.registry.get('inventory');
-        
         this.recipes = new Recipes();
 
-        // Listen for hotbar click events
         this.game.events.on('hotbarClicked', this.onHotbarClicked, this);
-
         this.initialiseStoveMenu();        
     }
 
     initialiseStoveMenu(){
         const sizes = this.registry.get('sizes');
 
-        const menuX = 30;
-        const menuY = 30;
-        const menuWidth = sizes.width - 60;
-        const menuHeight = sizes.height - 120;
+        const measurements = {
+            menuX: 30,
+            menuY: 30,
+            menuWidth: sizes.width - 60,
+            menuHeight: sizes.height - 120,
+            zoneX: 80,
+            zoneY: 100,
+            zoneWidth: 118,
+            zoneHeight: 70,
+            zoneGap: 120
 
-        // Add background and cancel button
-        this.add.rectangle(menuX, menuY, menuWidth, menuHeight, 0xfadde1).setOrigin(0, 0).setInteractive();
-        this.add.image(menuWidth + 10, menuY - 15, "cancel").setOrigin(0);
+        }
+        
+        this.select = this.add.image(measurements.zoneX, measurements.zoneY, 'select').setOrigin(0, 0);
 
-        // Makes the cancel button clickable
-        let cancelZone = this.add.zone(menuWidth + 10, menuY - 15, 42, 42).setOrigin(0, 0).setInteractive({ useHandCursor: true });
+        this.createBackgroundAndCancel(measurements);
+        this.createPan(measurements);
+        this.createZones(measurements);
 
-        // If cancel is clicked then close the scene and return all ingredients to inventory
+    }
+
+    /**
+     * Transfers the ingredient from the inventory to the stove if there are enough spaces on the stove.
+     * @param {number} zoneIndex 
+     */
+    onHotbarClicked(zoneIndex){
+        if (this.ingredients.length < 3 && !this.inventory.isEmptyAt(zoneIndex)){
+            let ingredientArray = this.inventory.getInventory()[zoneIndex];
+            this.inventory.removeIngredient(zoneIndex);
+            this.ingredients[zoneIndex] = [ingredientArray[0], ingredientArray[1]]; // Only add the name and icon of ingredient
+
+            this.questionText[this.selectedIndex].setVisible(false);
+            this.updateSelection();
+            this.updateStoveMenu();
+        }
+    }
+
+    /**
+     * Reset all the ingredient images and redisplays them.
+     */
+    updateStoveMenu(){
+        let ingX = 110;
+        let ingY = 100;
+        let gap = 238;
+
+        this.ingredients.forEach((ingredient, i) => {
+            if (ingredient[2] != null){
+                ingredient[2].destroy();
+                ingredient.pop();
+            }
+            ingredient.push(this.add.image(ingX, ingY, ingredient[1]).setOrigin(0, 0).setScale(2));
+            this.ingredients[i] = ingredient;
+            ingX += gap;
+        });
+    }
+
+    /**
+     * Increases the select image location.
+     */
+    updateSelection(){
+        if (this.selectedIndex < 3){
+            this.selectedIndex++;
+            this.select.setX(80 + this.selectedIndex * 238);
+        }
+    }
+
+    /**
+     * Checks if a recipe exists with the current ingredients on the stove. If it exists, then get the meal name
+     * and add the meal to your inventory. If it doesn't, add rocks to the inventory. Display an popup message for
+     * either outcomes and resets the scene.
+     */
+    cook(){
+        let recipe = {};
+        let ingredientNames = [];
+
+        this.ingredients.forEach(ing => {
+            ingredientNames.push(ing[0]);
+        });
+
+        if (this.recipes.findRecipe(ingredientNames, "Stove") != null){
+            recipe = this.recipes.findRecipe(ingredientNames, "Stove");
+            this.inventory.addIngredient(recipe.meal, recipe.mealIcon);
+
+            this.scene.get("Hotbar").showMessage(recipe.meal + "!!", "achievement")
+        }
+        else {
+            this.inventory.addIngredient("Rocks", "rocks");
+            this.scene.get("Hotbar").showMessage("Rocks...?", "achievement");
+        }
+
+        this.ingredients.forEach(ingredient => {
+            if (ingredient[2] != null){ ingredient[2].destroy(); }
+        });
+
+        this.ingredients = [];
+        this.updateStoveMenu();
+        this.scene.restart();
+    }
+
+    /**
+     * Initialises the background, cancel button, and label for the stove menu.
+     * @param {StoveMeasurements} measurements 
+     */
+    createBackgroundAndCancel(measurements){
+        this.add.rectangle(measurements.menuX, measurements.menuY, measurements.menuWidth, measurements.menuHeight, 0xfadde1).setOrigin(0, 0).setInteractive();
+        
+        this.add.image(measurements.menuWidth + 10, measurements.menuY - 15, "cancel").setOrigin(0);
+        let cancelZone = this.add.zone(measurements.menuWidth + 10, measurements.menuY - 15, 42, 42).setOrigin(0, 0).setInteractive({ useHandCursor: true });
+
         cancelZone.on('pointerdown', () => {
             if (this.selectedIndex > -1){
                 this.ingredients.forEach(ingredient => {
@@ -60,42 +166,43 @@ export default class StoveMenu extends Phaser.Scene {
             this.stove.close();
         });
 
-        // Add the label
         this.add.text(40, 40, 'Stove', {
             fontFamily: 'daydream',
             fontSize: '25px',
             color: '#000000',
             padding: { top: 4, bottom: 6 }
         });
+    }
 
-        
-
-        // Add the 3 ingredient zone locations
-        let zoneX = menuX + 50;
-        let zoneY = menuY + 70;
-        let zoneWidth = 118;
-        let zoneHeight = 70;
-        let zoneGap = 120;
-
-        this.select = this.add.image(zoneX, zoneY, 'select').setOrigin(0, 0);
-
-        // Add the frying pan
+    /**
+     * Intialises the pan animation and its zone.
+     * @param {StoveMeasurements} measurements 
+     */
+    createPan(measurements){
         if (!this.anims.exists('moving_pan')) { this.anims.createFromAseprite('pan'); }
 
-        let pan = this.add.sprite(menuWidth/2 - 30, menuHeight/2, "pan").setOrigin(0, 0).setScale(4);
+        let pan = this.add.sprite(measurements.menuWidth/2 - 30, measurements.menuHeight/2, "pan").setOrigin(0, 0).setScale(4);
         pan.play({ key: 'moving_pan', repeat: -1 });
 
-        let panZone = this.add.zone(menuWidth/2 - 30, menuHeight/2, 130, 130).setOrigin(0, 0).setInteractive({ useHandCursor: true });
+        let panZone = this.add.zone(measurements.menuWidth/2 - 30, measurements.menuHeight/2, 130, 130).setOrigin(0, 0).setInteractive({ useHandCursor: true });
         panZone.on('pointerdown', () => {
             if (this.ingredients.length == 3){
                 this.cook();
             }
             else {this.scene.get("Hotbar").showMessage("Not enough ingredients!!", "error")}
         });
+    }
+
+    /**
+     * Initialises the stores each of the ingredient zones.
+     * @param {StoveMeasurements} measurements 
+     */
+    createZones(measurements){
+        let zoneX = measurements.zoneX;
 
         for (let i = 0; i < 3; i++) {
-            let zone = this.add.zone(zoneX, zoneY, zoneWidth, zoneHeight).setOrigin(0, 0);
-            let text = this.add.text(zoneX + 25, zoneY + 16, "???",{
+            let zone = this.add.zone(zoneX, measurements.zoneY, measurements.zoneWidth, measurements.zoneHeight).setOrigin(0, 0);
+            let text = this.add.text(zoneX + 25, measurements.zoneY + 16, "???",{
                 fontFamily: 'daydream',
                 fontSize: '25px',
                 color: '#999999',
@@ -110,81 +217,7 @@ export default class StoveMenu extends Phaser.Scene {
 
             this.ingredientZones.push(zone);
             
-            zoneX += zoneWidth + zoneGap;
+            zoneX += measurements.zoneWidth + measurements.zoneGap;
         }
-
-    }
-
-    onHotbarClicked(zoneIndex){
-        // Only add if stove has < 3 ingredients
-        if (this.ingredients.length < 3 && !this.inventory.isEmptyAt(zoneIndex)){
-            // Grab the ingredient of the clicked zone, remove it from inventory, and add to the stove
-            let ingredientArray = this.inventory.getInventory()[zoneIndex];
-            this.inventory.removeIngredient(zoneIndex);
-            this.ingredients[zoneIndex] = [ingredientArray[0], ingredientArray[1]]; // Only add the name and icon of ingredient
-
-            // Make ??? invisible and move over the select image
-            this.questionText[this.selectedIndex].setVisible(false);
-            this.updateSelection();
-
-            this.updateStoveMenu();
-        }
-    }
-
-    updateStoveMenu(){
-        let ingX = 110;
-        let ingY = 100;
-        let gap = 238;
-
-        // Display all the stove ingredients
-        this.ingredients.forEach((ingredient, i) => {
-            // Destroy and redraw all the images
-            if (ingredient[2] != null){
-                ingredient[2].destroy();
-                ingredient.pop();
-            }
-            ingredient.push(this.add.image(ingX, ingY, ingredient[1]).setOrigin(0, 0).setScale(2));
-            this.ingredients[i] = ingredient;
-            ingX += gap;
-        });
-    }
-
-    updateSelection(){
-        if (this.selectedIndex < 3){
-            this.selectedIndex++;
-            this.select.setX(80 + this.selectedIndex * 238);
-        }
-    }
-
-    cook(){
-        // Check if there's a recipe for the ingredients
-        let recipe = {};
-        let ingredientNames = [];
-
-        this.ingredients.forEach(ing => {
-            ingredientNames.push(ing[0]);
-        });
-
-        if (this.recipes.findRecipe(ingredientNames, "Stove") != null){
-            recipe = this.recipes.findRecipe(ingredientNames, "Stove");
-            this.inventory.addIngredient(recipe.meal, recipe.mealIcon);
-
-            this.scene.get("Hotbar").showMessage(recipe.meal + "!!", "achievement")
-        }
-
-        // Add rocks to the inventory
-        else {
-            this.inventory.addIngredient("Rocks", "rocks");
-            this.scene.get("Hotbar").showMessage("Rocks...?", "achievement");
-        }
-
-        // Destroy all the ingredient images
-        this.ingredients.forEach(ingredient => {
-            if (ingredient[2] != null){ ingredient[2].destroy(); }
-        });
-        this.ingredients = [];
-        this.updateStoveMenu();
-
-        this.scene.restart();
     }
 }
